57993dae4c98e65dc528abf6757ed20e1870f8c3
Casper Karlsen <casperbkarlsen@gmail.com>
2024-10-15 16:02:26 +0200

    Ready for development

---
+++ frontend/build.gradle.kts
@@ -0,0 +1,22 @@
+import com.github.gradle.node.npm.task.NpmTask
+
+plugins {
+    id("com.github.node-gradle.node") version "7.1.0"
+}
+
+node {
+    version = "22.9.0"
+    npmVersion = "10.8.3"
+    download = true
+}
+
+tasks.register<NpmTask>("runBuild") {
+    args = listOf("run", "build")
+    workingDir = file(".")
+}
+
+tasks.register<Copy>("copyWebApp") {
+    from("dist")
+    into("../backend/src/main/resources/static")
+    dependsOn("runBuild")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,4 @@
 rootProject.name = "pollapp"
+include("backend")
+include("frontend")
+
