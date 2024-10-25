ce928e68435905178119053c645ba61d2fbcea53
Vetle Knutsen <vetleknutsen1@gmail.com>
2024-10-25 12:20:10 +0200

    made some changes in both backend and frontend

--- backend/src/main/java/gruppe3/pollapp/controllers/PollController.java
+++ backend/src/main/java/gruppe3/pollapp/controllers/PollController.java
@@ -29,4 +29,14 @@ public class PollController {
     }
 
 
+    @PostMapping(value = "/create_poll", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Poll> createPoll(@RequestBody Poll poll) {
+
+        System.out.println("Received poll: " + poll);
+
+
+        manager.addPoll(1, poll);
+        return ResponseEntity.ok(poll);
+    }
+
 }
